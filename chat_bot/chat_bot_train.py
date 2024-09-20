f9fe9fad1814f5c022dbaa15a91a1db694af3664
Park jaemin <hungryshark4292@gmail.com>
2024-09-20 16:40:51 +0900

    chat bot code

--- chat_bot/chat_bot_train.py
+++ chat_bot/chat_bot_train.py
@@ -1,45 +1,9 @@
+from transformers import T5ForConditionalGeneration, AutoTokenizer
 
-import os
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+model = T5ForConditionalGeneration.from_pretrained('google/byt5-small')
+tokenizer = AutoTokenizer.from_pretrained('google/byt5-small')
 
-model_id = 'MLP-KTLim/llama-3-Korean-Bllossom-8B'
+model_inputs = tokenizer(["Life is like a box of chocolates.", "Today is Monday."], padding="longest", return_tensors="pt")
+labels = tokenizer(["La vie est comme une boîte de chocolat.", "Aujourd'hui c'est lundi."], padding="longest", return_tensors="pt").input_ids
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-)
-
-model.eval()
-
-PROMPT = '''You are a helpful AI assistant. Please answer the user's questions kindly. 당신은 유능한 AI 어시스턴트 입니다. 사용자의 질문에 대해 친절하게 답변해주세요.'''
-instruction = "서울의 유명한 관광 코스를 만들어줄래?"
-
-messages = [
-    {"role": "system", "content": f"{PROMPT}"},
-    {"role": "user", "content": f"{instruction}"}
-    ]
-
-input_ids = tokenizer.apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    return_tensors="pt"
-).to(model.device)
-
-terminators = [
-    tokenizer.eos_token_id,
-    tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
-
-outputs = model.generate(
-    input_ids,
-    max_new_tokens=2048,
-    eos_token_id=terminators,
-    do_sample=True,
-    temperature=0.6,
-    top_p=0.9
-)
-
-print(tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True))
+loss = model(**model_inputs, labels=labels).loss # forward pass
